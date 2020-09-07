3b2f3687ff2f88b990304261208cacfb95096da0
Christopher Turton <christopheraturton@gmail.com>
2020-09-07 16:42:20 -0400

    Updates

---
+++ precache-manifest.5ef4844b77e9bbfd9fa0ebcb6c3947ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71710578a5ba9825b2401574722e5aa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "410397ab436cc3db0ed8",
+    "url": "/static/css/main.f8647641.chunk.css"
+  },
+  {
+    "revision": "d488c906c91819045375",
+    "url": "/static/js/2.3ce55a69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3ce55a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "410397ab436cc3db0ed8",
+    "url": "/static/js/main.2e44cc1a.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  }
+]);
